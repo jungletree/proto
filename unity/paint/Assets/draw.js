4ce98ef3e84e0efc34ca076b42ce432e01bee4e4
U-KMJP\e11655 <e11655@PN1IKSA26949A.kmjp.local>
2013-10-24 09:41:23 +0900

    only added todo comments

--- unity/paint/Assets/draw.js
+++ unity/paint/Assets/draw.js
@@ -1,9 +1,29 @@
 ﻿#pragma strict
 
+var texure : Texture2D;
+var penColor : Color.white;
+
 function Start () {
 
 }
 
 function Update () {
-
+	var pointer : Vector2 = Input.mousePosition;
+	
+	/*if(Input.GetKeyDown("mouse 0")) {
+		print("mouse down");
+	}
+	if(Input.GetKeyDown("t")) {
+		print("key t down");
+	}*/
+	if(Input.GetKey("mouse 0")) {
+		print("mouse: " + pointer.x + ", " + pointer.y);
+	}
+	if(Input.GetKeyUp("mouse 0")) {
+		print("mouse up");
+	}
+	
+	//TODO: convert pointing cordinate (screen) to canvas
+	//TODO: 
+	//TODO: update texture 
 }
