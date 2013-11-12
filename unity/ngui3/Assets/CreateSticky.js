b784b37cbbec5c87b7b21a79b799908cbae5e080
ogino <ogin0o@gmail.com>
2013-11-13 00:59:42 +0900

    added CreatedSticky.js

---
+++ unity/ngui3/Assets/CreateSticky.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+//TODO: debug and fix following. 
+//var sticky : StickySprite;
+function create() {
+    //Instantiate(sticky, Vector3(0, -150, 0), Quaternion.identity);
+    Debug.Log("clicked");
+}
