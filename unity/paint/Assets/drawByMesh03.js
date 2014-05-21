1c915d2b36eb1d104081c6939c1a57433ad4fcad
ogino <e11655@collage.local>
2014-05-21 17:41:52 +0900

    cleaned up comments.

--- unity/paint/Assets/drawByMesh03.js
+++ unity/paint/Assets/drawByMesh03.js
@@ -15,36 +15,15 @@ private var uvs : Array = new Array();
 private var triangles : Array = new Array();
 
 function Start () {
-	//var radius = 20;
     pointer = new Mesh();
 	pointer.vertices = [
-	/*
-		Vector3(0, 0, -1),  // 0
-		Vector3(radius*Mathf.Cos(0/4*Mathf.PI), radius*Mathf.Sin(0/4*Mathf.PI), -1),  // 1
-		Vector3(radius*Mathf.Cos(1/4*Mathf.PI), radius*Mathf.Sin(1/4*Mathf.PI), -1),  // 2
-		Vector3(radius*Mathf.Cos(2/4*Mathf.PI), radius*Mathf.Sin(2/4*Mathf.PI), -1),  // 3
-		Vector3(radius*Mathf.Cos(3/4*Mathf.PI), radius*Mathf.Sin(3/4*Mathf.PI), -1),  // 4
-		Vector3(radius*Mathf.Cos(4/4*Mathf.PI), radius*Mathf.Sin(4/4*Mathf.PI), -1),  // 5
-		Vector3(radius*Mathf.Cos(5/4*Mathf.PI), radius*Mathf.Sin(5/4*Mathf.PI), -1),  // 6
-		Vector3(radius*Mathf.Cos(6/4*Mathf.PI), radius*Mathf.Sin(6/4*Mathf.PI), -1),  // 7
-		Vector3(radius*Mathf.Cos(7/4*Mathf.PI), radius*Mathf.Sin(7/4*Mathf.PI), -1)   // 8
-		*/
 		Vector3(-pointerSize, pointerSize, -1), Vector3(pointerSize, pointerSize, -1),
 		Vector3(pointerSize, -pointerSize, -1), Vector3(-pointerSize, -pointerSize, -1)
 	];
 	pointer.uv = [
-	/*
-		Vector2(0.5, 0.5),
-	    Vector2(1, 0), Vector2(1, 1), Vector2(0, 1), Vector2(-1, 1),
-	    Vector2(-1, 0), Vector2(-1, -1), Vector2(0, -1), Vector2(1, -1)
-	    */
 	    Vector2(0, 0), Vector2(1, 0), Vector2(1, 1), Vector2(0, 1)
     ];
 	pointer.triangles = [
-	/*
-        0, 1, 2, 0, 2, 3, 0, 3, 4, 0, 4, 5, 0, 5, 6, 0, 6, 7, 0, 7, 8, 0, 8, 1
-        //0, 2, 1, 0, 3, 2, 0, 4, 3, 0, 5, 4, 0, 6, 5, 0, 7, 6, 0, 8, 7, 0, 1, 8,
-        */
         0, 1, 2, 2, 3, 0
     ];
 
@@ -102,12 +81,9 @@ function Update () {
 			} else {
 				// do nothing ...
 			}
-			//print("segments: " + segments + ", point: " + hit.point + ", vertices.length: " + mesh.vertices.length + ", uv.length: " + mesh.uv.length + ", triangles.length: " + mesh.triangles.length);
-
 			Graphics.DrawMesh(pointer, curr + transform.position, Quaternion.identity, pointerMaterial, 0);
 		}
 	}
 	Graphics.DrawMesh(strokes, transform.position , Quaternion.identity, strokeMaterial, 0);
-	//Graphics.DrawMesh(mesh, Vector3.zero , Quaternion.identity, null, 0);
 }
 
